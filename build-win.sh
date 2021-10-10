70f32af2fc5d1f16efcd5ebc727bd146f209792c
mikel evins <mikel@evins.net>
2021-10-10 09:14:48 -0500

    tweaks to the build script

--- build-win.sh
+++ build-win.sh
@@ -2,7 +2,7 @@
 
 #/c/acl10.1.64/alisp --qq -L lisp/appserver.asd -e "(cl-user::buildapp)" --kill
 
-/c/Program Files/Steel Bank Common Lisp/2.0.0/sbcl.exe --dynamic-space-size 4gb --no-userinit --load lisp/appserver.asd --eval "(cl-user::buildapp)"
+/c/Program\ Files/Steel\ Bank\ Common\ Lisp/2.0.0/sbcl.exe --dynamic-space-size 4gb --no-userinit --load lisp/appserver.asd --eval "(cl-user::buildapp)"
 
 # test command that leaves ACL console windows up for debugging purposes:
 #  /c/acl10.1.64/alisp -p --qq -L lisp/appserver.asd -e "(cl-user::buildapp)"
